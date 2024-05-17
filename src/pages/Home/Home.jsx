065e7963fe1d211ad5640c56ef95593787b5f162
miasumon <miasumon.ru@gmail.com>
2024-05-17 09:32:13 +0600

    banner added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 
-
+import Banner from "./Banner/Banner";
 
 const Home = () => {
     return (
         <div>
-
-        <h1 className="text-4xl"> This is Home page</h1>
+            <Banner></Banner>
         </div>
     );
 };
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const  Navbar = () => {
 
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar  fixed z-10 bg-black text-white max-w-7xl  bg-opacity-30">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
