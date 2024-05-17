bf50554ed1061fb3eb01cc33a433a570daea37b8
miasumon <miasumon.ru@gmail.com>
2024-05-17 09:02:09 +0600

    navbar added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/shared/Footer/Footer";
+import Navbar from "../pages/shared/Navbar/Navbar";
 
 
 const MainLayout = () => {
     return (
         <div>
 
+            <Navbar></Navbar>
+
             <Outlet></Outlet>
+
+            <Footer></Footer>
             
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,13 @@ import router from './routes/Routes.jsx';
 
 
 
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <div className='max-w-7xl mx-auto'>
+
+      <RouterProvider router={router} />
+
+    </div>
+
   </React.StrictMode>,
 )
